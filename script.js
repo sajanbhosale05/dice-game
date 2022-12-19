63317e90a693a7f47f69cda1f8a6ce5333142a9f
sajanbhosale05 <83913731+sajanbhosale05@users.noreply.github.com>
2022-12-20 00:18:02 +0530

    Initial commit

--- script.js
+++ script.js
@@ -23,10 +23,28 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+
+//Initial page Load
+init();
+
 //Switching the Player
 
 const switchPlayer = function () {
@@ -82,4 +100,4 @@ btnHold.addEventListener("click", function () {
   }
 });
 
-btnNew.addEventListener("click", function () {});
+btnNew.addEventListener("click", init);
